c458759aa623da42b634ddbdd9d0beaa357150ba
Felipe Salazar <j.felipesatr@gmail.com>
2023-05-26 19:01:44 -0500

    update

--- zPics.js
+++ zPics.js
@@ -6,7 +6,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.tileLayer(
-  'https://api.gbif.org/v2/map/occurrence/density/{z}/{x}/{y}@1x.png?taxonKey=2442225&bin=hex&hexPerTile=70&style=iNaturalist.poly',
+  'https://api.gbif.org/v2/map/occurrence/density/{z}/{x}/{y}@1x.png?taxonKey=4504&bin=hex&hexPerTile=70&style=blue.marker',
   {
     maxZoom: 0,
   }
